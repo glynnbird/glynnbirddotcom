c8f78806d760b915d8a7ae5a0c255659b1a4de03
Glynn Bird <glynn.bird@gmail.com>
2021-03-12 10:21:07 +0000

    remove syntax highlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,4 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
 module.exports = function(eleventyConfig) {
   // copy static assets straight through to final site
   eleventyConfig.addPassthroughCopy('assets')
-  eleventyConfig.addPlugin(syntaxHighlight);
 }
