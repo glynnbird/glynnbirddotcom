4f388f44a891868c1e5afd8ef5e0bc6f13acc69c
Glynn Bird <glynn.bird@gmail.com>
2021-03-12 10:27:36 +0000

    add CNAME to rendered site

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
   // copy static assets straight through to final site
   eleventyConfig.addPassthroughCopy('assets')
+  eleventyConfig.addPassthroughCopy('CNAME')
 }
