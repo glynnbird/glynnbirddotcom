5264a3036c83b9dcf5f73b653c997994949b4e9a
Glynn Bird <glynn.bird@gmail.com>
2023-07-12 14:26:31 +0100

    inline styling please

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://glynnbird.com'
+  site: 'https://glynnbird.com',
+  build: {
+    inlineStylesheets: 'always'
+  }
 });
