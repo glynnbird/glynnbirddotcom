c7d913fd8c09131f3ea1e4197b747681cd5d6e4f
Glynn Bird <glynn.bird@gmail.com>
2024-10-25 16:14:54 +0100

    s3 state

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,11 +1,15 @@
 terraform {
+  backend "s3" {
+    bucket = "grb-terraform-state"
+    key    = "glynnbirddotcom/terraform.tfstate"
+    region = "eu-west-1"
+  }
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 4.42.0"
     }
   }
-
   required_version = ">= 1.3"
 }
 
